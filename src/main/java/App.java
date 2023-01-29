94c14cb40f5ff2137178132c4b19420774dd6654
Alexandr <kulagin.alexandr.net@gmail.com>
2023-01-29 09:25:39 +0300

    Переимнование переменных

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,15 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld bean2 =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        Cat cat1 =
-                (Cat) applicationContext.getBean("cat");
-        Cat cat2 =
-                (Cat) applicationContext.getBean("cat");
-        System.out.println(bean == bean2);
-        System.out.println(cat1 == cat2);
+        HelloWorld beanHello1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld beanHello2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat beanCat1 = applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(beanHello1 == beanHello2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
